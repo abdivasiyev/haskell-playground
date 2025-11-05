3860c29220755053c358c0659cd3f6c2f4b1d26b
Asliddin Abdivasiyev <asliddin.abdivasiyev@gmail.com>
2025-11-05 13:55:37 +0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Haskell playground";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    devshell,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        localSystem = {inherit system;};
+        overlays = [
+          devshell.overlays.default
+        ];
+      };
+      hpkgs = pkgs.haskell.packages."ghc912";
+
+      playground = pkgs.haskell.lib.overrideCabal (hpkgs.callCabal2nix "playground" ./. {}) (old: {
+        doCheck = true;
+        doHaddock = false;
+        enableLibraryProfiling = false;
+        enableExecutableProfiling = false;
+      });
+    in {
+      packages.default = playground;
+
+      devShells.default = pkgs.devshell.mkShell {
+        packages = [
+          hpkgs.cabal-install
+          hpkgs.cabal-add
+          hpkgs.haskell-language-server
+          hpkgs.fourmolu
+          hpkgs.ghc
+        ];
+      };
+    });
+}
